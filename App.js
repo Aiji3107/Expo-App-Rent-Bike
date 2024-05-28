fbf7358fda3c69fe9ae8a974b51d0fa814e166c2
Aiji3107 <aghikalam2@gmail.com>
2024-05-28 18:53:45 +0700

    edit import login

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './src/Navigator/Navigation';
-import Login from './src/login/login';
+import Login from './src/Login/login';
 import { auth } from './src/firebase/firebase';
 
 const App = () => {
